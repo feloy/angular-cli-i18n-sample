bbd22846d5e0e4e258a6700a149b5a6cac0198dc
angular-cli <angular-cli@angular.io>
2017-01-05 08:51:52 +0100

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularCliI18nSamplePage } from './app.po';
+
+describe('angular-cli-i18n-sample App', function() {
+  let page: AngularCliI18nSamplePage;
+
+  beforeEach(() => {
+    page = new AngularCliI18nSamplePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
